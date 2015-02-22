a2d64dccc8ed7c772bd05ba361479529935193eb
Karl Cauchy <karlcauchy@gmail.com>
2015-02-22 20:14:36 +0800

    Updated read me for this package

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,14 @@
-/*
- * grunt-update-readme
- * https://github.com/kcauchy/grunt-update-readme
- *
- * Copyright (c) 2015 Karl Cauchy
- * Licensed under the MIT license.
- */
+module.exports = function(grunt){
 
-'use strict';
-
-module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
+   grunt.initConfig({
     update_readme: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
+      'package.json': {
+         'readme': 'README.md'
       }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
-
   });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	grunt.loadNpmTasks('grunt-update-readme');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'update_readme', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
-
+	grunt.registerTask('default', ['update_readme']);
 };
